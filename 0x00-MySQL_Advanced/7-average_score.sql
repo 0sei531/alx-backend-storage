7ee4f6bef30a81529559b41c1907ae9f06da923e
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-11 10:47:58 -1200

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,13 @@
+-- Task 7: Average score - creates a stored procedure ComputeOverallScoreForUser
+-- that computes and store the overall score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);  -- Adjust precision as needed
-
-    -- Compute average score
-    SELECT AVG(score) INTO avg_score 
-    FROM corrections 
-    WHERE user_id = user_id;
-
-    -- Handle cases where there are no corrections
-    IF avg_score IS NULL THEN
-        SET avg_score = 0;  -- or NULL as per your application logic
-    END IF;
-
-    -- Update users table with the computed average score
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
-
-    COMMIT;  -- Commit transaction
-END $$
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
