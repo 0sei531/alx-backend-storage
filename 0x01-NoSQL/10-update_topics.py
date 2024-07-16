ae60a252943093eea63510f47390915d1b1ce343
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-16 00:52:19 -1200

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,26 +2,14 @@
 """ Write a Python function that changes all topics of a school
     document based on the name
 """
+
+
 def update_topics(mongo_collection, name, topics):
-    """
-    Update topics of all school documents that match the name.
-
-    Args:
-        mongo_collection (pymongo.collection.Collection):
-        The collection object to update.
-        name (str): The name of the school whose topics are to be updated.
-        topics (list of str): The new list of topics to set for the school.
-
-    Returns:
-        None
+    """ mongo_collection will be the pymongo collection object
+        - name (string) will be the school name to update
+        - topics (list of strings) will be the list of topics
+          approached in the school
     """
     query = {"name": name}
     new_values = {"$set": {"topics": topics}}
-
-    # Check if there are documents matching the query
-    count = mongo_collection.count_documents(query)
-    if count == 0:
-        print(f"No documents found with name '{name}' to update.")
-        return
-
     mongo_collection.update_many(query, new_values)
