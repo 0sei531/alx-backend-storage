5a4ef0adf90827af34db2e5e1edc84fd714041d7
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-16 09:48:16 -1200

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -19,12 +19,23 @@ if __name__ == "__main__":
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
+
     print(f'{status_check} status check')
 
     top_ips = nginx_collection.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+         },
         {"$sort": {"count": -1}},
-        {"$limit": 10}
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
     ])
 
     print("IPs:")
