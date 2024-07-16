79164875946c00f624c672ef58320aa41c3e7f97
0sei531 <oseikofiantwi531@gmail.com>
2024-07-16 14:28:01 -0700

    a

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """ Write a Python script that provides some stats about
     Nginx logs stored in MongoDB
@@ -14,13 +13,14 @@ if __name__ == "__main__":
 
     n_logs = nginx_collection.count_documents({})
     print(f'{n_logs} logs')
+
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+        print(f"\tmethod {method}: {count}")
 
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
-        )
+    )
     print(f'{status_check} status check')
