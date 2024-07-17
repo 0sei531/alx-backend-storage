f4c6eb83810dd04f8db2acb455384b9a29f436bf
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-17 09:07:42 -1200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
-""" Module for Implementing an Expiring Web Cache and Tracker using Redis """
+""" Advanced - Module for Implementing an expiring
+    web cache and tracker
+"""
 
-from functools import wraps
 import redis
 import requests
 from typing import Callable
+from functools import wraps
 
-# Initialize Redis connection
-redis_conn = redis.Redis()
+rd = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """Decorator for counting how many times a request has been made"""
+    """ Counting with decorators how many times a request
+        has been made
+    """
 
     @wraps(method)
-    def wrapper(url: str) -> str:
-        """Wrapper for decorator functionality"""
-        redis_conn.incr(f"count:{url}")
-        cached_html = redis_conn.get(f"cached:{url}")
+    def wrapper(url):
+        """ Wrapper for decorator functionality """
+        rd.incr(f"count:{url}")
+        cached_html = rd.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
 
         html = method(url)
-        redis_conn.setex(f"cached:{url}", 10, html)
+        rd.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
@@ -30,11 +33,8 @@ def count_requests(method: Callable) -> Callable:
 
 @count_requests
 def get_page(url: str) -> str:
-    """Fetch the HTML content of a particular URL"""
+    """ requests module to obtain the HTML
+        content of a particular URL and returns it.
+    """
     req = requests.get(url)
     return req.text
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    print(get_page(url))
-    print(get_page(url))  # This should fetch from cache
