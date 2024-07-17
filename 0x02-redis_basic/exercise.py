9c4d902292590202b7601b2ad29cb2e7500ff5c6
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-17 09:25:48 -1200

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -68,16 +68,16 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-   def get(self, key: str,
-        fn: Optional[Callable] = None
-        ) -> Union[str, bytes, int, float, None]:
-    """Reading from Redis and recovering original type."""
-    value = self._redis.get(key)
-    if value is None:
-        return None
-    if fn:
-        return fn(value)
-    return value
+    def get(self, key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        """Reading from Redis and recovering original type."""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
 
     def get_str(self, key: str) -> Optional[str]:
         """Parameterizes a value from Redis to str."""
